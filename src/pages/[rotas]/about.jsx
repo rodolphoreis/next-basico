6b30f0889a41464408173f3b20a02567532cf741
Rodolpho Reis <rodolphoreis@live.com>
2024-06-10 17:36:03 +0100

    feat: create route with Link

--- src/pages/[rotas]/about.jsx
+++ src/pages/[rotas]/about.jsx
@@ -1,5 +1,16 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 const about = () => {
-  return <div></div>;
+  const router = useRouter();
+
+  return (
+    <div>
+      <h1>About</h1>
+      <Link href={"/rotas/home"}>
+        <button>Voltar</button>
+      </Link>
+    </div>
+  );
 };
 
 export default about;
