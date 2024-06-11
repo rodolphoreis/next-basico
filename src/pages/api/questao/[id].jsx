03e1a5b65fc6edb137b6344bed7f34f036d126e1
Rodolpho Reis <rodolphoreis@live.com>
2024-06-11 20:56:41 +0100

    feat: create GET

--- src/pages/api/questao/[id].jsx
+++ src/pages/api/questao/[id].jsx
@@ -1,1 +1,12 @@
-export default (req, res) => {};
+export default (req, res) => {
+  try {
+    if (req.method === "GET") {
+      res.status(200).json({
+        enunciado: "Qual é a sua cor preferida?",
+        resposta: ["Branco", "Vermelho", "Azul", "Preto", "Amarelo"],
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Erro na resposta.", error: true });
+  }
+};
