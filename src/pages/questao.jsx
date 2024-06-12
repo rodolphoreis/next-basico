5bf79b7f5e20c48a5078f7ec1785f7d76ed60f1f
Rodolpho Reis <rodolphoreis@live.com>
2024-06-12 19:08:17 +0100

    feat: create function

--- src/pages/questao.jsx
+++ src/pages/questao.jsx
@@ -1,22 +1,38 @@
 import { useEffect, useState } from "react";
 
-const questao = () => {
-  const [questao, setQuestao] = useState();
+const Questao = () => {
+  const [questao, setQuestao] = useState({});
 
   useEffect(() => {
     fetch("http://localhost:3000/api/questao/123")
-      .then((res) => res.json())
-      .then(setQuestao);
-
-    console.log("questao: ", questao);
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Não existe nenhuma questão!");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log("Dados recebidos da API:", data);
+        setQuestao(data);
+      })
+      .catch((err) => console.error("Erro ao buscar a questão:", err));
   }, []);
+
+  const alternativas = () => {
+    if (questao && questao.resposta) {
+      return questao.resposta.map((resp, i) => <li key={i}>{resp}</li>);
+    } else {
+      return null;
+    }
+  };
 
   return (
     <div>
-      <h2>questao</h2>
+      <h2>Questão</h2>
       <span>{questao?.enunciado}</span>
+      <ul>{alternativas()}</ul>
     </div>
   );
 };
 
-export default questao;
+export default Questao;
