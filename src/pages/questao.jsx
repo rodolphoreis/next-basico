c099ddf71acd1202e40a9b19156c8808310e3468
Rodolpho Reis <rodolphoreis@live.com>
2024-06-12 17:35:01 +0100

    feat: add then and catch

--- src/pages/questao.jsx
+++ src/pages/questao.jsx
@@ -1,5 +1,13 @@
 const questao = () => {
   fetch("http://localhost:3000/api/questao/123")
+    .then((res) => res.json())
+    .then((json) => console.log(json));
+
+  return (
+    <div>
+      <h2>questao</h2>
+    </div>
+  );
 };
 
 export default questao;
