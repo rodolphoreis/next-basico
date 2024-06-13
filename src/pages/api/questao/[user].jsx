005d62152d5f62fb68c04e0eee86ea41e18ecdbb
Rodolpho Reis <rodolphoreis@live.com>
2024-06-13 17:02:56 +0100

    refactor in GET

--- src/pages/api/questao/[user].jsx
+++ src/pages/api/questao/[user].jsx
@@ -1,11 +1,17 @@
 export default (req, res) => {
   if (req.method === "GET") {
-    const id = req.query.user;
-    res.status(200).json({
-      id,
-      nome: "Rodolpho",
-      idade: 34,
-      morada: "Rua do Dev",
-    });
+    Get(req, res);
+  } else {
+    res.status(405).send();
   }
+};
+
+const Get = (req, res) => {
+  const id = req.query.user;
+  res.status(200).json({
+    id,
+    nome: "Rodolpho",
+    idade: 34,
+    morada: "Rua do Dev",
+  });
 };
