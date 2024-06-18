8f31267d2d5aed2cd4f8241e2fc69179a059014b
Rodolpho Reis <rodolphoreis@live.com>
2024-06-18 17:41:08 +0100

    feat: create state

--- src/pages/form.jsx
+++ src/pages/form.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+
 export default function form() {
+  const [nome, setNome] = useState("");
+  const [idade, setIdade] = useState("");
+
   return (
     <div>
       <h1>Form</h1>
