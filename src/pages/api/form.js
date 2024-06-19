966e52c9b8d1112fdc33c61c14e1bd558e2690e2
Rodolpho Reis <rodolphoreis@live.com>
2024-06-19 16:25:12 +0100

    feat: create method get

--- src/pages/api/form.js
+++ src/pages/api/form.js
@@ -1,4 +1,9 @@
 export default function form(req, res) {
   if (req.method === "POST") {
     post(req, res);
+  } else if (req.method === "GET") {
+    get(req, res);
+  } else {
+    res.status(405).send();
+  }
 }
